1d1625643c0388564c814153b0bba4468de64e0a
Ben-Worrall <124666132+Ben-Worrall@users.noreply.github.com>
2023-02-13 22:54:47 +0200

    Update rate.js

--- rate.js
+++ rate.js
@@ -79,26 +79,9 @@ var items = [
 
 
 
-//preload images to cache
-function preloadImages(array) {
-    if (!preloadImages.list) {
-        preloadImages.list = [];
-    }
-    var list = preloadImages.list;
-    for (var i = 0; i < array.length; i++) {
-        var img = new Image();
-        img.onload = function() {
-            var index = list.indexOf(this);
-            if (index !== -1) {
-                
-            }
-        }
-        list.push(img);
-        img.src = array[i];
-    }
-}
-
-preloadImages(items);
+v$(items).each(function() {
+var image = $('<img />').attr('src', this);
+});
 
 
 
